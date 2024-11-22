c03bb52dbf8fbf4cb21b74f72e2acff9a5cf8495
idris-adigun <idris.adigun@ymail.com>
2024-11-22 12:18:06 -0500

    Update default SUS_FLAG environment variable to remove 'R'

--- nids/config/env.py
+++ nids/config/env.py
@@ -6,7 +6,7 @@ sus_ip = os.getenv('SUS_IP', '192.168.1.1,10.0.0.1,172.16.0.1')
 sus_dst_port = os.getenv('SUS_PORT', '44206')
 sus_src_port = os.getenv('SUS_PORT', '44206')
 sus_payload = os.getenv('SUS_PAYLOAD', 'attack,exploit,password')
-sus_flag = os.getenv('SUS_FLAG', 'S,A,P,R')
+sus_flag = os.getenv('SUS_FLAG', 'S,A,P')
 
 sus_ip = sus_ip.split(',')
 sus_dst_port = sus_dst_port.split(',')
